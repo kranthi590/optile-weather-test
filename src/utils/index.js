699744a4d2cbd07ba10780965dff1e8df833406e
Kranthi Kumar Pasala <pasala.kk@gmail.com>
2019-07-10 15:56:31 -0400

    Wrote httpHandler with unit test cases

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,21 @@
-export const httpHandler = () => {
-  return 'hello';
+import axios from 'axios';
+
+export const httpHandler = (options = {}) => {
+  return new Promise((resolve, reject) => {
+    if (!options.url) {
+      throw new Error('URL_MISSING');
+    }
+    axios({
+      method: options.method || 'GET',
+      url: options.url,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      resolve(response.data);
+    }).catch((error) => {
+      reject(error);
+    });
+  });
+
 };
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,15 +1,32 @@
 import { API_URL } from '../constants';
-import {httpHandler} from './index';
+import { httpHandler } from './index';
 
 describe('Test Suite for utils', () => {
 
-  it('Should get data from API', async () => {
-    const options = {
-      url: API_URL,
-      method: 'GET'
-    };
-    const response = httpHandler(options);
+  const options = {
+    url: API_URL,
+    method: 'GET'
+  };
+
+  it('Should get 200 status from API', async () => {
+    const response = await httpHandler(options);
     expect(response).toBeDefined();
+  });
+
+  it('Should throw `URL_MISSING` error when url not passed', (done) => {
+    httpHandler().catch((error) => {
+      expect(error).toBeDefined();
+      expect(error.message).toBe('URL_MISSING');
+      done();
+    });
+  });
+
+  it('Should reject with error when invalid url is passed', (done) => {
+    httpHandler({ url: 'http://locahost:1111' }).catch((error) => {
+      expect(error).toBeDefined();
+      expect(error.code).toBe('ENOTFOUND');
+      done();
+    });
   });
 
 });
