d5eb5987560ab759d7173cbf9b94cfa1063bc311
Kranthi Kumar <kranthi.pasala@hayneedle.com>
2020-01-28 14:03:50 -0300

    changed to https

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,4 +6,4 @@ export const TEMP_TYPES = {
 
 export const DISPLAY_CARDS = 3;
 
-export const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40';
+export const API_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40';
